c1ad27e1115d4942514774f0240cccd25641aa2c
Kaifei Chen <kaifei@berkeley.edu>
2017-08-25 16:53:41 -0700

    Add a SERIAL_NUMBER flag
    - add a SERIAL_NUMBER flag for "make flash"
    - SERIAL_NUMBER can be accessed by running "make list-ttys"

--- makefiles/tools/edbg.inc.mk
+++ makefiles/tools/edbg.inc.mk
@@ -3,7 +3,11 @@ EDBG ?= $(RIOT_EDBG)
 FLASHER ?= $(EDBG)
 OFLAGS ?= -O binary
 HEXFILE = $(ELFFILE:.elf=.bin)
-FFLAGS ?= $(EDBG_ARGS) -t $(EDBG_DEVICE_TYPE) -b -e -v -p -f $(HEXFILE)
+ifeq ($(SERIAL_NUMBER),)
+  FFLAGS ?= $(EDBG_ARGS) -t $(EDBG_DEVICE_TYPE) -b -e -v -p -f $(HEXFILE)
+else
+  FFLAGS ?= $(EDBG_ARGS) -t $(EDBG_DEVICE_TYPE) -b -e -v -p -f $(HEXFILE) -s $(SERIAL_NUMBER)
+endif
 
 ifeq ($(RIOT_EDBG),$(FLASHER))
   FLASHDEPS += $(RIOT_EDBG)
